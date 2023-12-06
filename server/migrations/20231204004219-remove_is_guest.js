aa36391cae9c5292a27f15a72ffb24a0e44bbb26
harshagarine <garineharshavardhan@gmail.com>
2023-12-05 18:16:05 -0800

    Auth files

---
+++ server/migrations/20231204004219-remove_is_guest.js
@@ -0,0 +1,18 @@
+module.exports = {
+  async up(db, client) {
+
+    const collection = db.collection('users');
+
+    await collection.updateMany({}, { $unset: { is_guest: false } });
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  }
+};
